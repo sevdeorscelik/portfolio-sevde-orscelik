09c2ae0b97736fb3443eee537c833f9861100dc5
Sevde <sevdeorscelik@gmail.com>
2022-11-10 21:32:58 +0100

    changed deutsch

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ce5067df97a3c87c"; }
+/******/ 		__webpack_require__.h = function() { return "2667400862772875"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
