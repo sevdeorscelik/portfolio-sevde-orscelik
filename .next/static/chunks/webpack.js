d35aba452ca17aa9628a7ffc9f7fe87222a07cb9
Sevde <sevdeorscelik@gmail.com>
2022-11-29 22:01:15 +0100

    added demo-link on teknoza

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d2c794ff5f7c489d"; }
+/******/ 		__webpack_require__.h = function() { return "6a4d96c7d31f5bae"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.a79afa60441b41a6.hot-update.js
+++ .next/static/webpack/webpack.a79afa60441b41a6.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "23ba705443e417f8"; }
+/******/ 	__webpack_require__.h = function() { return "83000d2697a8bab5"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
